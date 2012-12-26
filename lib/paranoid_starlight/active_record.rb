4e1cfdeda5b2dea9e202f54cbad5a1fba33153df
Ivan Stana <stiipa@centrum.sk>
2012-12-26 22:58:18 +0100

    Bug in ActiveRecord hook, forgot renaming of Validations

--- lib/paranoid_starlight/active_record.rb
+++ lib/paranoid_starlight/active_record.rb
@@ -3,6 +3,6 @@
 require_relative "../paranoid_starlight.rb"
 
 require 'active_record'
-::ActiveRecord::Base.send(:include, ParanoidStarlight::Validators)
+::ActiveRecord::Base.send(:include, ParanoidStarlight::Validations)
 ::ActiveRecord::Base.send(:include, ParanoidStarlight::Converters)
 
